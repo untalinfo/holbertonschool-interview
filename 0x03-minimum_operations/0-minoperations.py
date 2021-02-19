bea26c0b3630bf3879289caa6cd66b9665707521
Oskar Info <racso1607@gmail.com>
2021-02-18 23:32:23 -0500

    first commit

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+calculates the minimum amount of processes necessary
+to create a certain number of characters.
+'''
+
+
+def minOperations(n):
+    p = 0
+
+    if n <= 1:
+        return p
+
+    for i in range(2, n + 1):
+        while (0 == n % i):
+            p = p + i
+            n = n / i
+            if n < i:
+                break
+    return p
