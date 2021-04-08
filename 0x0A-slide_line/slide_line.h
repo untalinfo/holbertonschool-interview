91d703aca4d857294fcf0591d152f35915f228f3
Oskar Info <racso1607@gmail.com>
2021-04-07 23:34:01 -0500

    first commit

---
+++ 0x0A-slide_line/slide_line.h
@@ -0,0 +1,13 @@
+#ifndef SLIDE_H
+#define SLIDE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
+
+int slide_line(int *line, size_t size, int direction);
+
+#endif
