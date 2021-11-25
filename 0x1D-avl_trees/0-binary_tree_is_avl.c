75c64c957a334f47b587d2efedbdd48052926ac4
untalinfo <racso1607@gmail.com>
2021-11-25 08:29:38 -0500

    fix betty style

--- 0x1D-avl_trees/0-binary_tree_is_avl.c
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -10,18 +10,18 @@
 */
 int is_avl(const binary_tree_t *root, int min, int max, int *height)
 {
-	int left_height = 0, right_height = 0;
+    int left_height = 0, right_height = 0;
 
-	if (root == NULL)
-		return (1);
-	if (root->n < min || root->n > max)
-		return (0);
-	if (!is_avl(root->left, min, root->n - 1, &left_height))
-		return (0);
-	if (!is_avl(root->right, root->n + 1, max, &right_height))
-		return (0);
-	*height = 1 + (left_height > right_height ? left_height : right_height);
-	return (abs(left_height - right_height) <= 1);
+    if (root == NULL)
+        return (1);
+    if (root->n < min || root->n > max)
+        return (0);
+    if (!is_avl(root->left, min, root->n - 1, &left_height))
+        return (0);
+    if (!is_avl(root->right, root->n + 1, max, &right_height))
+        return (0);
+    *height = 1 + (left_height > right_height ? left_height : right_height);
+    return (abs(left_height - right_height) <= 1);
 }
 
 /**
@@ -32,9 +32,9 @@ int is_avl(const binary_tree_t *root, int min, int max, int *height)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	int height = 0;
+    int height = 0;
 
-	if (!tree)
-		return (0);
-	return (is_avl(tree, INT_MIN, INT_MAX, &height));
+    if (!tree)
+        return (0);
+    return (is_avl(tree, INT_MIN, INT_MAX, &height));
 }
