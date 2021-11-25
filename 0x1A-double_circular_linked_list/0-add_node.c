29feb9ed8aa94e4a6e6ededaad87573b7cfa02cb
untalinfo <racso1607@gmail.com>
2021-11-25 08:24:50 -0500

    initial commit

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -1,66 +1,66 @@
 #include "list.h"
 
-/**
-* create_node - creates a new node
-* @str: data for a new node
-* Return: a new node or NULL if failed
-*/
-List *create_node(char *str)
-{
-	List *node;
-
-	node = malloc(sizeof(*node));
-	if (!node)
-		return (NULL);
-	node->str = strdup(str);
-	if (!node->str)
-	{
-		free(node->str);
-		return (NULL);
-	}
-	node->next = node->prev = NULL;
-	return (node);
-}
 
 /**
-* add_node_end - add a node at the end of circular linked list
-* @list: pointer to a list
-* @str: data for a new node
-* Return: a new node or NULL if failed
+ * add_node_end - Add a new node to the end of a double circular linked list
+ * @list: list to be modified
+ * @str: the string to copy into the new node
+ * Return: Address of the new node, or NULL on failure
 */
+
 List *add_node_end(List **list, char *str)
 {
-	List *end, *node;
+	List *new_node = NULL;
 
-	if (!list || !str)
-		return (NULL);
-	node = create_node(str);
-	if (!node)
-		return (NULL);
-	if (*list)
+	new_node = malloc(sizeof(List));
+	if (new_node == NULL)
 	{
-		end = (*list)->prev;
-		end->next = node;
-		node->prev = end;
-		node->next = *list;
-		(*list)->prev = node;
-	}
-	else
-	{
-		*list = node;
-		node->prev = node->next = node;
+		return (new_node);
 	}
 
-	return (node);
+	/**
+	 * The strdup() and strndup() functions
+	 * are used to duplicate a string.
+	 * Returns: A pointer to a null-terminated byte string, which
+	 * is a duplicate of the string pointed to by s.
+	*/
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		return (NULL);
+	}
+
+	if ((*list) == NULL)
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		(*list) = new_node;
+		return (new_node);
+	}
+
+	new_node->next = (*list);
+	new_node->prev = (*list)->prev;
+	(*list)->prev = new_node;
+	new_node->prev->next = new_node;
+	return (new_node);
 }
 
 /**
-* add_node_begin - add a node at the beginning of circular linked list
-* @list: pointer to a list
-* @str: data for a new node
-* Return: a new node or NULL if failed
+ * add_node_begin - Add a new node to the beginning of a double circular
+ * linked list
+ * @list: list to be modify
+ * @str: string to copy into the new node
+ * Return: Address of the new node, or NULL on failure.
 */
+
 List *add_node_begin(List **list, char *str)
 {
-	return (add_node_end(list, str) ? *list = (*list)->prev : NULL);
+	List *new_node = add_node_end(list, str);
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	(*list) = new_node;
+	return (new_node);
 }
